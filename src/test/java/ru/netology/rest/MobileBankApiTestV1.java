fc12efcdb64281ec409a67a2d2d7bd7d020cf56a
zaikadm94 <zaikadm94@mail.ru>
2023-07-04 00:14:43 +0300

    add error in test

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -17,7 +17,7 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(200)
+          .statusCode(400)
         .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
