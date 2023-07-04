c9934c2496319853fed8c2b5c098861fcf70e695
zaikadm94 <zaikadm94@mail.ru>
2023-07-04 15:30:52 +0300

    fix test and fix config

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -17,7 +17,7 @@ class MobileBankApiTestV1 {
           .get("/demo/accounts")
       // Проверки
       .then()
-          .statusCode(400)
+          .statusCode(200)
         .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
